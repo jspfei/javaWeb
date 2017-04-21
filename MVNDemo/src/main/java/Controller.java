bd638b1c51247a6b2e2baae2b77c79c9b2c57c7b
jspfei <jiangfeifan8@163.com>
2017-04-21 12:18:25 +0800

    xianj

---
+++ MVNDemo/src/main/java/Controller.java
@@ -0,0 +1,27 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by admin on 2017/4/21.
+ */
+public class Controller extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        System.err.println("---");
+        //解决乱码
+        req.setCharacterEncoding("UTF-8");
+        String name=req.getParameter("name");
+        req.setAttribute("name",name);
+        System.out.println(name);
+        req.getRequestDispatcher("index.jsp").forward(req, resp);
+    }
+
+}
